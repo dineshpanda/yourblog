59a0759d9012687b833686e4b4847bcc4b564ca6
Dinesh Panda <dineshpanda92@gmail.com>
2020-02-07 01:53:34 +0530

    Generate posts resource with draft:resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
+  # Routes for the Post resource:
+
+  # CREATE
+  get("/posts/new", { :controller => "posts", :action => "new_form" })
+  post("/create_post", { :controller => "posts", :action => "create_row" })
+
+  # READ
+  get("/posts", { :controller => "posts", :action => "index" })
+  get("/posts/:id_to_display", { :controller => "posts", :action => "show" })
+
+  # UPDATE
+  get("/posts/:prefill_with_id/edit", { :controller => "posts", :action => "edit_form" })
+  post("/update_post/:id_to_modify", { :controller => "posts", :action => "update_row" })
+
+  # DELETE
+  get("/delete_post/:id_to_remove", { :controller => "posts", :action => "destroy_row" })
+
+  #------------------------------
+
   # Routes for the Article resource:
 
   # CREATE
