722a566e00e057ff07e0472f221550f7d1e305c6
Dinesh Panda <dineshpanda92@gmail.com>
2020-02-07 01:53:31 +0530

    Generate articles resource with draft:resource

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,59 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+
+    render("article_templates/index.html.erb")
+  end
+
+  def show
+    @article = Article.find(params.fetch("id_to_display"))
+
+    render("article_templates/show.html.erb")
+  end
+
+  def new_form
+    @article = Article.new
+
+    render("article_templates/new_form.html.erb")
+  end
+
+  def create_row
+    @article = Article.new
+
+
+    if @article.valid?
+      @article.save
+
+      redirect_back(:fallback_location => "/articles", :notice => "Article created successfully.")
+    else
+      render("article_templates/new_form_with_errors.html.erb")
+    end
+  end
+
+  def edit_form
+    @article = Article.find(params.fetch("prefill_with_id"))
+
+    render("article_templates/edit_form.html.erb")
+  end
+
+  def update_row
+    @article = Article.find(params.fetch("id_to_modify"))
+
+
+    if @article.valid?
+      @article.save
+
+      redirect_to("/articles/#{@article.id}", :notice => "Article updated successfully.")
+    else
+      render("article_templates/edit_form_with_errors.html.erb")
+    end
+  end
+
+  def destroy_row
+    @article = Article.find(params.fetch("id_to_remove"))
+
+    @article.destroy
+
+    redirect_to("/articles", :notice => "Article deleted successfully.")
+  end
+end
