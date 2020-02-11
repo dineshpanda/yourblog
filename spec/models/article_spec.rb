a5a3c21ec6196eff4a254684bc70adbe10345a57
Dinesh Panda <dineshpanda92@gmail.com>
2020-02-11 12:45:12 +0530

    6th draft

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Article, type: :model do
   
     describe "Direct Associations" do
 
+    it { should belong_to(:user) }
+
     end
 
     describe "InDirect Associations" do
@@ -11,6 +13,12 @@ RSpec.describe Article, type: :model do
     end
 
     describe "Validations" do
+
+    it { should validate_uniqueness_of(:title).case_insensitive }
+
+    it { should validate_presence_of(:title) }
+
+    it { should validate_length_of(:title).is_at_least(2).is_at_most(200) }
       
     end
 end
